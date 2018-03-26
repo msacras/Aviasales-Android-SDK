bdda678584fd71c6614158819118a90dcbb51d9c
cmaxim <xscrub96x@gmail.com>
2018-03-26 15:28:21 +0300

    - internal browser removed - code cleaned up

--- aviasalestemplate/src/main/java/ru/aviasales/template/utils/BrowserUtils.java
+++ aviasalestemplate/src/main/java/ru/aviasales/template/utils/BrowserUtils.java
@@ -7,9 +7,9 @@ import android.os.Bundle;
 import android.provider.Browser;
 import android.support.annotation.Nullable;
 
-import ru.aviasales.template.ui.fragment.BrowserFragment;
-
 public class BrowserUtils {
+	public static final String REFERER_HEADER = "Referer";
+	public static final String HTTP = "http://";
 
 	public static void openExternalBrowser(@Nullable Activity activity, @Nullable String url, @Nullable String host) {
 		if (activity == null || url == null) return;
@@ -17,7 +17,7 @@ public class BrowserUtils {
 		intent.setData(Uri.parse(url));
 		if (host != null) {
 			Bundle bundle = new Bundle();
-			bundle.putString(BrowserFragment.REFERER_HEADER, BrowserFragment.HTTP + host);
+			bundle.putString(REFERER_HEADER, HTTP + host);
 			intent.putExtra(Browser.EXTRA_HEADERS, bundle);
 		}
 		activity.startActivity(intent);
